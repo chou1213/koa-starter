5602514f5ac3b7579ef29450142189e2ac788e1a
chenliqiong <joan.zhou2@gmail.com>
2024-08-07 00:20:32 +0800

    feat: 修改读取环境变量的逻辑

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
-const Koa = require('koa')
+const Koa = require('koa');
+const fs = require('fs');
 const bodyParser = require('koa-bodyparser');
 const { config } = require('dotenv');
-const router = require('./routes/index');
 const { fail } = require('./utils/index');
 const authMiddleware = require('./middlewares/authMiddleware')
 
-config({ path: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env' })
+const defaultEnvPath = [];
+const envPath = `.env.${process.env.NODE_ENV}`;
+
+// 检查特定环境的 .env 文件是否存在
+if (fs.existsSync(envPath)) {
+  defaultEnvPath.push(envPath)
+}
+
+config({ path: defaultEnvPath, override: true })
 
 const app = new Koa();
 const port = process.env.PORT || 3000;
 const SECRET_KEY = process.env.SECRET_KEY;
+
+const router = require('./routes/index');
 
 app
   .use(authMiddleware(SECRET_KEY, { path: [/^\/user/] }))
